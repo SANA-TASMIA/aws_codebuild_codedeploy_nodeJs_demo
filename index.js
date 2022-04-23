d0d82bcf11549f92a80feabfc45a215baa07ad0a
sana <sanatasmia8@gamil.com>
2022-04-23 12:03:40 +0530

    v4 auto declarative

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V3 for declarative!')
+  res.send('CICD App V4 for declarative!')
 })
 
 app.get('/status', (req, res) => {
