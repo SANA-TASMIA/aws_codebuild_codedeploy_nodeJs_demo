a66ea160a0395306774479cc185aab51959832de
sana <sanatasmia8@gamil.com>
2022-04-24 11:09:12 +0530

    v6 for npm build stage

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V6 for npm build stage!')
+  res.send('CICD App V7 for npm build stage!')
 })
 
 app.get('/status', (req, res) => {
